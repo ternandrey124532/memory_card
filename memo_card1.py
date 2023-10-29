929a60e93bfa020fd807dfb9a79801f61fa5b1cd
ternandrey124532 <146370343+ternandrey124532@users.noreply.github.com>
2023-10-29 18:49:42 +0200

    Add files via upload

--- memo_card1.py
+++ memo_card1.py
@@ -1,40 +1,86 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import*
-vidp=QPushButton('Відповісти')
-men=QPushButton('Меню')
-otdih=QPushButton('Відпочити')
-boxm=QSpinBox()
-boxm.setValue(30)
-groupb=QGroupBox('Варіанти відповідей')
-gp=QButtonGroup()
+from PyQt5.QtCore import Qt#функциональный модуль
+from PyQt5.QtWidgets import*#За окно
+app=QApplication([])#создание приложения
+vidp=QPushButton('Відповісти')#кнопка отвеат
+men=QPushButton('Меню')#кнопка меню
+otdih=QPushButton('Відпочити')#кнопка отдыха
+boxm=QSpinBox()#таймер окошко
+boxm.setValue(30)#начальное колличество секунд
+
+box1=QGroupBox('Варіанти відповідей')#создание бокса1
+vopros=QLabel('')#создание текста вопроса
+gp=QButtonGroup()#группа баттонов
 #knopki
-ct=QRadioButton()
-aplle=QRadioButton()
-appl=QRadioButton()
-build=QRadioButton()
+radio1=QRadioButton()#создание радиобаттона
+radio2=QRadioButton()#создание радиобаттона
+radio3=QRadioButton()#создание радиобаттона
+radio4=QRadioButton()#создание радиобаттона
 
-gp.addButton(ct)
-gp.addButton(apple)
-gp.addButton(appl)
-gp.addButton(build)
+gp.addButton(radio1)#добавление кнопки радиобаттона
+gp.addButton(radio2)#добавление кнопки радиобаттона
+gp.addButton(radio3)#добавление кнопки радиобаттона
+gp.addButton(radio4)#добавление кнопки радиобаттона
+#вертикальные
+v1=QVBoxLayout()#создание вертикальных линий
+v2=QVBoxLayout()#создание вертикальных линий
+v3=QVBoxLayout()#главная вертикальная линия
+v4=QVBoxLayout()#создание вертикальных линий
+#виджеты кнопок
+v1.addWidget(radio1)#добавление виджета на 1 линию
+v1.addWidget(radio2)#добавление виджета на 1 линию
+v2.addWidget(radio3)#добавление виджета на 2 линию
+v2.addWidget(radio4)#добавление виджета на 2 линию
+#прикрепили радиобаттони к боксу1
+horizontal=QHBoxLayout()#горизонтальная линия
+horizontal.addLayout(v1)#добавление 1 линии
+horizontal.addLayout(v2)#добавление 2 линии
+box1.setLayout(horizontal)#добавление в бокс1 горизонтальную линию которая содержит в себе 2 вертикальх
 
-v1=QVBoxLayout()
-v2=QVBoxLayout()
-v3=QVBoxLayout()
-v4=QVBoxLayout()
+#горизонтальная
+g1=QHBoxLayout()#создание горизонтальной линии
+g2=QHBoxLayout()#создание горизонтальной линии
+g3=QHBoxLayout()#создание горизонтальной линии
+g4=QHBoxLayout()#создание горизонтальной линии
 
-v1.addWidget(ct)
-v1.addWidget(aplle)
-v2.addWidget(appl)
-v2.addWidget(build)
+g1.addWidget(men)#добавление виджета меню на 1 горизонтальную линию
+g1.addWidget(otdih)#добавление виджет отдых на 1 линию
 
-g1=QHBoxLayout()
-g1.addLayout(men)
-g1.addLayout(otdih)
+g2.addWidget(vopros)#добавления виджета вопроса
 
-g2=QHBoxLayout()
-g2.addLayout(v1)
-g2.addLayout(v2)
+g3.addWidget(box1)#добаавления на 3 линию виджет бокс1
 
-g3=QHBoxLayout()
-g3.add(vidp)
+g4.addWidget(vidp)#добавление на 4 линию ответа
+
+#box2
+box2=QGroupBox('результат')#создание 2 бокса
+rightw=QLabel()#праивльно не правильно
+vidpovid=QLabel()#apple
+vres=QVBoxLayout()#вертикальная линия
+vres.addWidget(rightw,alignment=(Qt.AlignTop | Qt.AlignLeft))
+vres.addWidget(vidpovid,alignment=Qt.AlignCenter,stretch=2)
+box2.setLayout(vres)#добавление бокс2 на вертикальную линию
+g3.addWidget(box2)#добавление на горизотальную линию бокса2
+box2.hide()#бокс2 не показівается
+
+
+v3.addLayout(g1)#добавление на главную линию горизонтальную
+v3.addLayout(g2)#добавление на главную линию горизонтальную
+v3.addLayout(g3)#добавление на главную линию горизонтальную
+v3.addLayout(g4)#добавление на главную линию горизонтальную
+
+
+
+def showr():#нажал відповісти
+    box1.hide()#!!!!!!!!!!!
+    box2.show()#бокс2 показівается
+    vidp.setText('Наступне питання')#установка текста следуюий вопрос
+def showq():#функция показ вопроса
+    vidp.setText('Відповисти')#ставим текст ответить
+    box1.show()#бокс1 показывается
+    box2.hide()#бокс2 спрятался
+    gp.setExclusive(False)#снятие огранчиений что б скинуть выбор
+    radio1.setChecked(False)#снятие ограничений с радиобаттона 1
+    radio2.setChecked(False)#снятие ограничений с радиобаттона 2
+    radio3.setChecked(False)#снятие ограничений с радиобаттона 3
+    radio4.setChecked(False)#снятие ограничений с радиобаттона 4
+    gp.setExclusive(True)#вернуть ограничения
--- memo_main.py
+++ memo_main.py
@@ -1,12 +1,97 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import*
-w,h=600,500
-app=QApplication([])
-window=QtWidget
-window.resize(w,h)
-window.setWindowTitle("Memory Card")
+from PyQt5.QtCore import Qt#функциональный модуль
+from random import*#подключение рандома
+from PyQt5.QtWidgets import*#За окно
+from PyQt5.QtCore import QTimer#timer
+from memo_card1 import*#модуль 
+from question_layout import*#подклчение линий из модуля
+#from random import*
+time_unit=60000#1 minyta
+timer=QTimer()#создание таймера
+def sleep():#функция для остановки таймера
+    window.hide()#окно зачиняється
+    schet=boxm.text()#считывание текста 
+    schet=int(schet)#переделывание в числовой формат 
+    timer.setInterval(schet*time_unit)#создание интервала
+    timer.start()#таймер запускается
+    timer.timeout.connect(wake)
+def wake():#функция для запуска таймера
+    timer.stop()#таймер осстанавливается
+    window.show()#окно показывается
+
+w,h=600,500#ширина и высота окна
+#tre=True
+window=QWidget()#создание виджета
+window.resize(w,h)#установка своих размеров окна
+window.setWindowTitle("Memory Card")#установка названия на окно
 window.move(100,100)
 
+        
+frm_question='Яблуко'#вопрос
+frm_right='apple'#правильный ответ
+frm_wrong1='application'#неправильный ответ
+frm_wrong2='building'#неправильный ответ
+frm_wrong3='caterpillar'#неправильный ответ
 
-window.show()
-app.exec_()
+radio_l=[radio1,radio2,radio3,radio4]#создание списка с радиобаттонами
+#shuffle(radio_l)
+
+radio_l[0].setText(frm_right)#ставим правильный ответ на радиоаттон
+radio_l[1].setText(frm_wrong3)#ставим не правильный ответ
+radio_l[2].setText(frm_wrong1)#ставим не правильный ответ
+radio_l[3].setText(frm_wrong2)#ставим не правильный ответ
+vidpovid.setText(frm_right)#ставим правильный ответ
+vopros.setText(frm_question)#ставим вопрос
+
+def chek_result():#функция для проверки результата
+    correct=radio_l[0].isChecked()#проверка правильного ответа
+    if correct:
+        rightw.setText('правильно')#меняем текст на правильно
+        showr()#нажатие на кнопку ответить
+    else:#или
+        incorrect=radio_l[1].isChecked() or radio_l[2].isChecked() or radio_l[3].isChecked()
+        if incorrect:
+            rightw.setText('неправильно')#текст неправильно 
+            showr()#нажатие на кнопку ответить
+def click_OK():#функция для нажатия
+    if vidp.text() == 'Відповісти':#считывание текста
+        chek_result()#проверка результата
+qlist=[]#создание списка
+def creatq():#создание вопроса
+    a=qe1.text()#считывание текста 
+    b=qe2.text()#считывание текста
+    c=qe3.text()#считывание текста
+    d=qe4.text()#считывание текста
+    e=qe5.text()#считывание текста
+    g=Question(a,b,c,d,e)#добавление вопросов в клас
+    qlist.append(g)#добавление вопросов в список
+def clear():#очистка вопросов
+    qe1.clear()#очистка вопросов
+    qe2.clear()#очистка вопросов
+    qe3.clear()#очистка вопросов
+    qe4.clear()#очистка вопросов
+    qe5.clear()#очистка вопросов
+
+def start():#кнопка запуск
+    randq=choise(qlist)#рандом вопросов в списке
+    radio_l[0].setText(randq.answer)#правильний вопрос
+    radio_l[1].setText(randq.wrong_answer1)#неправильный вопрос
+    radio_l[2].setText(randq.wrong_answer2)#неправильный вопрос
+    radio_l[3].setText(randq.wrong_answer3)#неправильный вопрос
+    vidpovid.setText(randq.answer)#правильный ответ
+    vopros.setText(randq.question)#вопрос
+    
+
+
+window.setLayout(v3)#крепим главную вертикальну линию на окно
+window2.setLayout(vrtg)#крепим главную горизонтальную линию на окно
+vidp.clicked.connect(click_OK)#нажатие на ответить
+
+qb1.clicked.connect(creatq)#создание вопросов
+qb2.clicked.connect(clear)#очистка вопросов
+qb3.clicked.connect(start)#запуск
+
+window.show()#окно показывается
+window2.show()#окно показывается
+window.hide()#окно закрыть
+app.exec_()#приложение закрыть
+app.exec_()#приложение закрыть
